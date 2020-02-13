2133c67dcfe4099836b5147386d3f06010837de8
wer <werner.e.2.g@gmail.com>
2020-02-12 20:20:49 -0600

    Type of data

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-console.log("Hola");
-document.write("Hola");
+//tipos de datos
+"Hola" //string
+'Hola' //string
 
+//number
+10.3
+-2.4
+
+//boolean
+true
+false
+
+//array
+['Armando','paredes']
+[1,2,4]
+[true,false,true,false]
+
+// objetc
+true
+{
+ "username": "wer",
+ "score" : 95.9,
+ "hours": 8,
+ "proffesional": true
+}
